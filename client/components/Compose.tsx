7d0eaee4fcc5cafb12ba760fd54742c02c9ad001
Joel Tewhare <joeltewhare@gmail.com>
2026-01-01 20:32:57 +1300

    Setup styling structure in compose component

--- client/components/Compose.tsx
+++ client/components/Compose.tsx
@@ -1,3 +1,10 @@
 export default function Compose() {
-  return <div>Compose</div>
+  return (
+    <div className="min-h-screen w-full bg-email-grey p-4">
+      <div className="flex flex-col gap-6 md:flex-row">
+        <div className="w-full md:w-80">Options here</div>
+        <div className="w-full flex-1">Prompt and text area here</div>
+      </div>
+    </div>
+  )
 }
