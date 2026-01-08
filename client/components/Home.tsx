4f5d9f962874a2a3bf0bb0b545b24eb1c1166739
Joel Tewhare <joeltewhare@gmail.com>
2026-01-08 19:06:47 +1300

    Add intro graphic to homepage

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -4,6 +4,16 @@ import { Link } from 'react-router'
 export default function Home() {
   return (
     <div className="flex w-full flex-col items-center justify-center">
+      <div className="flex h-screen flex-row items-center justify-center gap-4">
+        <img
+          src="/assets/images/icon-tabs-group-graphic.svg"
+          alt="icons graphic"
+        />
+        <img
+          src="/assets/images/landing-page-text-graphic.svg"
+          alt="landing page logo"
+        />
+      </div>
       <Button
         asChild
         className="m-4 rounded-xl bg-email-charcoal px-4 py-3 text-email-white hover:bg-email-charcoal/80 hover:shadow-md"
