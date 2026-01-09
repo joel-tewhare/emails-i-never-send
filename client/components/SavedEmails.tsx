324560418c0c403a0b1ae8eefbdfb7c034b96df6
Joel Tewhare <joeltewhare@gmail.com>
2026-01-09 21:47:12 +1300

    Add email key

--- client/components/SavedEmails.tsx
+++ client/components/SavedEmails.tsx
@@ -1,7 +1,32 @@
+import { Card, CardContent } from '@/components/ui/card'
+
 export default function SavedEmails() {
   return (
     <div className="flex w-full flex-col items-center justify-center">
-      <h1>Saved Emails</h1>
+      <Card className="max-w-xxl m-4 rounded-none border-none bg-email-white">
+        <div className="flex flex-row justify-center">
+          <CardContent className="flex flex-row space-x-2 p-3 text-sm font-bold">
+            <div className="h-4 w-4 rounded-sm bg-email-blue"></div>
+            <p>Work</p>
+          </CardContent>
+          <CardContent className="flex flex-row space-x-2 p-3 text-sm font-bold">
+            <div className="h-4 w-4 rounded-sm bg-email-gold"></div>
+            <p>Relationships</p>
+          </CardContent>
+          <CardContent className="flex flex-row space-x-2 p-3 text-sm font-bold">
+            <div className="h-4 w-4 rounded-sm bg-email-mauve"></div>
+            <p>Customer Service</p>
+          </CardContent>
+          <CardContent className="flex flex-row space-x-2 p-3 text-sm font-bold">
+            <div className="h-4 w-4 rounded-sm bg-email-mint"></div>
+            <p>Emotional Honesty</p>
+          </CardContent>
+          <CardContent className="flex flex-row space-x-2 p-3 text-sm font-bold">
+            <div className="h-4 w-4 rounded-sm border border-email-charcoal/50 bg-email-white"></div>
+            <p>Conflict Resolution</p>
+          </CardContent>
+        </div>
+      </Card>
     </div>
   )
 }
