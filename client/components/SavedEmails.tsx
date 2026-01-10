7ba63cfe286a02ca7af42de4b1633f1864636656
Joel Tewhare <joeltewhare@gmail.com>
2026-01-10 16:28:24 +1300

    Add useQuery for saved emails, hardcode user id

--- client/components/SavedEmails.tsx
+++ client/components/SavedEmails.tsx
@@ -1,6 +1,27 @@
 import { Card, CardContent } from '@/components/ui/card'
+import { getSavedEmails } from '../apis/saved-emails'
+import { useQuery } from '@tanstack/react-query'
 
 export default function SavedEmails() {
+  const userId = 1 //hardcoded until auth0 setup
+
+  const { data, isPending, error } = useQuery({
+    queryKey: ['emails'],
+    queryFn: () => getSavedEmails(userId),
+  })
+
+  if (isPending) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error loading data</div>
+  }
+
+  if (!data) {
+    return <div>No data available</div>
+  }
+
   return (
     <div className="flex w-full flex-col items-center justify-center">
       <Card className="max-w-xxl m-4 rounded-none border-none bg-email-white">
