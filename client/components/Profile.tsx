3cb6048ddfa8b0263ec843230c3481cd0cce9d47
Joel Tewhare <joeltewhare@gmail.com>
2026-01-13 13:09:16 +1300

    Edit profile details

--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -28,18 +28,16 @@ export default function Profile() {
           alt="icons graphic"
         />
         <div className="flex h-[500px] w-[400px] flex-col items-center border border-email-charcoal p-4">
-          <h1 className="my-3 w-full text-center text-3xl font-bold">
+          <h1 className="my-6 w-full text-center text-3xl font-bold">
             Your Profile
           </h1>
           <div className="ml-10 mt-6 w-full text-left">
             <h2 className="my-3 text-xl">Username</h2>
-            <p className="text-lg italic">username</p>
-            <h2 className="my-3 text-xl">First Name</h2>
-            <p className="text-lg italic">first name</p>
-            <h2 className="my-3 text-xl">Last Name</h2>
-            <p className="text-lg italic">last name</p>
+            <p className="text-lg italic">{data.username}</p>
+            <h2 className="my-3 text-xl">Full Name</h2>
+            <p className="text-lg italic">{`${data.firstName} ${data.lastName}`}</p>
           </div>
-          <Button className="mt-16 rounded-xl bg-email-charcoal px-4 py-3 text-lg text-email-white hover:bg-email-charcoal/80 hover:shadow-md">
+          <Button className="mt-24 rounded-xl bg-email-charcoal px-4 py-3 text-lg text-email-white hover:bg-email-charcoal/80 hover:shadow-md">
             Edit Profile
           </Button>
         </div>
