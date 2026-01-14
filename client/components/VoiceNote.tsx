590f1dd202491934296c5bb3b8df7505f6cb777a
Joel Tewhare <joeltewhare@gmail.com>
2026-01-14 18:35:21 +1300

    Install react audio recorder package, add states for audio, preview, status, remaining time, add function to store blob once recorded, add UI controls

--- client/components/VoiceNote.tsx
+++ client/components/VoiceNote.tsx
@@ -1,7 +1,40 @@
+import { useState } from 'react'
+import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder'
+
+type RecorderStatus = 'idle' | 'recording' | 'finalizing' | 'recorded' | 'error'
+
 export default function VoiceNote() {
+  const [audio, setAudio] = useState<Blob | null>(null)
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null)
+  const [recorderStatus, setRecorderStatus] = useState<RecorderStatus>('idle')
+  const [remainingSeconds, setRemainingSeconds] = useState<number>(20)
+
+  const recorderControls = useAudioRecorder(
+    {
+      noiseSuppression: true,
+      echoCancellation: true,
+    },
+    (error) => console.error('Error initializing recorder:', error),
+  )
+  const addAudioElement = (blob: Blob) => {
+    setAudio(blob)
+    if (previewUrl) {
+      URL.revokeObjectURL(previewUrl)
+    }
+    setPreviewUrl(URL.createObjectURL(blob))
+    setRecorderStatus('recorded')
+  }
+
   return (
     <div>
-      <h1>Voice Note</h1>
+      <AudioRecorder
+        onRecordingComplete={(blob) => addAudioElement(blob)}
+        recorderControls={recorderControls}
+        showVisualizer={true}
+      />
+      <button onClick={recorderControls.stopRecording}>Stop Recording</button>
+      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+      {previewUrl && <audio src={previewUrl} controls />}
     </div>
   )
 }
