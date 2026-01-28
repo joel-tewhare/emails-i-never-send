530110045916eafb86f288194a813513619bcf5d
Joel Tewhare <joeltewhare@gmail.com>
2026-01-28 19:47:41 +1300

    Add model type for setup of grounding doc

---
+++ models/setup.ts
@@ -0,0 +1,5 @@
+export type SetupAnswers = {
+  priority: { choice: string; other?: string | null }
+  avoid: { choice: string; other?: string | null }
+  tone: { choice: string; other?: string | null }
+}
