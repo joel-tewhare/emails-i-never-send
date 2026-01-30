bd394a5e4155414fe3f03ad7632e8c45cde48107
Joel Tewhare <joeltewhare@gmail.com>
2026-01-30 20:20:18 +1300

    Update index file to skip import of dotend in production

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,19 @@
-import 'dotenv/config'
-import server from './server.ts'
+async function loadEnv() {
+  if (process.env.NODE_ENV !== 'production') {
+    const dotenv = await import('dotenv')
+    dotenv.config()
+  }
+}
 
-const PORT = process.env.PORT || 3000
-
-server.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('Server listening on port', PORT)
-})
+loadEnv()
+  .then(async () => {
+    const { default: server } = await import('./server.ts')
+    const PORT = process.env.PORT || 3000
+    server.listen(PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log('Server listening on port', PORT)
+    })
+  })
+  .catch((err) => {
+    console.error('Failed to load dotenv: ', err)
+  })
